b6e60671d6859227c2f4dc2d88994944001d6a2c
Duxz <semada15@gmail.com>
2021-02-12 13:48:07 +0700

    change name for makesense

--- src/reaction/reaction.dto.ts
+++ src/reaction/reaction.dto.ts
@@ -2,7 +2,7 @@ import { Type, Transform } from 'class-transformer';
 import { Types } from 'mongoose';
 import { Reaction, ReactionType } from './reaction.schema';
 
-export class CreateReaction implements Partial<Reaction> {
+export class CreateReactionArgs implements Partial<Reaction> {
   upVote?: Types.ObjectId[]
 
   downVote?: Types.ObjectId[]

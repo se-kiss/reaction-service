4a276f97bc2e1516a246a6974b34c4c0f779561f
Duxz <semada15@gmail.com>
2021-02-12 13:06:47 +0700

    init reaction module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ReactionModule } from './reaction/reaction.module';
 
 @Module({
-  imports: [],
+  imports: [ReactionModule],
   controllers: [AppController],
   providers: [AppService],
 })

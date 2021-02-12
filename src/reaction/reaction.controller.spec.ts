98240740b9bbb1935de6efb337044587b581f939
Duxz <semada15@gmail.com>
2021-02-12 15:32:58 +0700

    implement controller & new upVote downVote controller

--- src/reaction/reaction.controller.spec.ts
+++ src/reaction/reaction.controller.spec.ts
@@ -1,18 +1,50 @@
+import { ConfigModule } from '@nestjs/config';
+import { RpcException } from '@nestjs/microservices';
+import { MongooseModule, getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { Types } from 'mongoose';
 import { ReactionController } from './reaction.controller';
+import { CreateReactionArgs, UpdateReactionArgs, VoteArgs } from './reaction.dto';
+import { Reaction, ReactionSchema, ReactionType } from './reaction.schema';
+import { ReactionService } from './reaction.service';
 
 describe('ReactionController', () => {
   let controller: ReactionController;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot(),
+        MongooseModule.forRoot(process.env.MONGODB_URL),
+        MongooseModule.forFeature([
+          {
+            name: Reaction.name,
+            schema: ReactionSchema
+          }
+        ])
+      ],
       controllers: [ReactionController],
+      providers: [ReactionService],
     }).compile();
 
-    controller = module.get<ReactionController>(ReactionController);
+    controller = module.get<ReactionController>(ReactionController)
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('should be upVote', async () => {
+    const mockReaction: CreateReactionArgs = {
+      reactionType: ReactionType.COMMENT
+    }
+    const mock = await controller.createReaction(mockReaction)
+    const data: VoteArgs = {
+      userId: new Types.ObjectId,
+      targetId: mock._id
+    }
+    const res = await controller.upVote(data)
+    console.log(res)
+    expect(res.upVote).toHaveLength(mock.upVote.length + 1)
+  })
 });
--- src/reaction/reaction.controller.ts
+++ src/reaction/reaction.controller.ts
@@ -1,4 +1,64 @@
-import { Controller } from '@nestjs/common';
+import { Controller, NotFoundException } from '@nestjs/common';
+import { GrpcMethod, RpcException } from '@nestjs/microservices';
+import { CreateReactionArgs, ReactionId, UpdateReactionArgs, VoteArgs } from './reaction.dto';
+import { ReactionService } from './reaction.service';
+import { status } from 'grpc';
 
 @Controller('reaction')
-export class ReactionController {}
+export class ReactionController {
+  constructor(private readonly reactionService: ReactionService) {}
+
+  @GrpcMethod('ReactionService', 'CreateReaction')
+  async createReaction(args: CreateReactionArgs) {
+    return await this.reactionService.create(args);
+  }
+
+  @GrpcMethod('ReactionService', 'GetReactions')
+  async getReactions() {
+    return await this.reactionService.gets({})
+  }
+
+  @GrpcMethod('ReactionService', 'DeleteReaction')
+  async deleteReaction({ _id }: ReactionId) {
+    try {
+      return await this.reactionService.delete(_id);
+    } catch (e) {
+      if (e instanceof NotFoundException) {
+        throw new RpcException({
+          code: status.NOT_FOUND,
+          message: _id.toHexString(),
+        });
+      }
+    }
+  }
+
+  @GrpcMethod('ReactionService', 'UpVote')
+  async upVote(args: VoteArgs) {
+    try {
+      const target = await this.reactionService.gets({ ids: [args.targetId] })
+      target[0].upVote.push(args.userId)
+      return await this.reactionService.update({ _id: args.targetId , upVote: target[0].upVote})
+    } catch (e) {
+      if (e instanceof NotFoundException) {
+        throw new RpcException({
+          code: status.NOT_FOUND,
+        });
+      }
+    }
+  }
+
+  @GrpcMethod('ReactionService', 'DownVote')
+  async downVote(args: VoteArgs) {
+    try {
+      const target = await this.reactionService.gets({ ids: [args.targetId] })
+      target[0].downVote.push(args.userId)
+      return await this.reactionService.update({ _id: args.targetId , downVote: target[0].downVote})
+    } catch (e) {
+      if (e instanceof NotFoundException) {
+        throw new RpcException({
+          code: status.NOT_FOUND,
+        });
+      }
+    }
+  }
+}

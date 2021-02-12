4bbebe117ef890dc25275243674adf8a2f648395
Duxz <semada15@gmail.com>
2021-02-12 13:40:14 +0700

    init service & inject schema

--- src/reaction/reaction.service.ts
+++ src/reaction/reaction.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Reaction } from './reaction.schema';
 
 @Injectable()
-export class ReactionService {}
+export class ReactionService {
+  constructor(
+    @InjectModel(Reaction.name)
+    private readonly reactionModel: Model<Reaction>,
+  ) {}
+}

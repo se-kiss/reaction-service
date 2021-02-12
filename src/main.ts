02609d1044a432e2d94824c23630d0c5cc63186a
Duxz <semada15@gmail.com>
2021-02-12 13:35:22 +0700

    change to microservice

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,37 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import {
+  MicroserviceOptions,
+  RpcException,
+  Transport,
+} from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import { status } from 'grpc';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('ReactionService');
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        url: '0.0.0.0:5000',
+        package: ['reaction'],
+        protoPath: ['reaction.proto'],
+      },
+    }
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      exceptionFactory: errors =>
+        new RpcException({
+          code: status.INVALID_ARGUMENT,
+          message: errors.toString(),
+        }),
+    }),
+  );
+  app.listen(() => logger.log('Reaction service is listening'));
 }
 bootstrap();

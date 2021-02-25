6a3b055233e53a92327ef5a8bc4850d25200afd4
Duxz <semada15@gmail.com>
2021-02-25 11:18:48 +0700

    init notification

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,18 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReactionModule } from './reaction/reaction.module';
+import { NotificationService } from './notification/notification.service';
+import { NotificationController } from './notification/notification.controller';
+import { NotificationModule } from './notification/notification.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGODB_URL),
-    ReactionModule
+    ReactionModule,
+    NotificationModule
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, NotificationController],
+  providers: [AppService, NotificationService],
 })
 export class AppModule {}
